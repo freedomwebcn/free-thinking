97ef1c070cd66a0d5707c1d9360359123be44723
freedomwebcn <freedomweb@yeah.net>
2022-11-09 11:16:22 +0800

    add css box-shadow

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -70,6 +70,7 @@ import authorList from "@/assets/author.json";
           object-fit: cover;
           margin-right: 12px;
           flex: none;
+          box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .2);
         }
 
         div {
