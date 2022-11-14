71df267f75dd88eba08949a2fc3997477d1d13b3
freedomwebcn <freedomweb@yeah.net>
2022-11-14 18:03:02 +0800

    add data

--- src/page/MagazineInfo.vue
+++ src/page/MagazineInfo.vue
@@ -1,109 +1,93 @@
 <template>
-    <div class="container">
-        <h1>本期目录</h1>
-        <div class="dec-content">
-            <ul>
-                <li v-for="item in decList" :key="item.illustrate">
-                    <div class="title van-hairline--bottom">
-                        <h2>{{ item.illustrate }}</h2>
-                    </div>
-                    <div class="van-hairline--bottom common dec-item" v-for="content in item.dec_content">
-                        {{ content.title }}
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </div>
+	<div class="container">
+		<h1>本期目录</h1>
+		<div class="dec-content">
+			<ul>
+				<li v-for="item in decList" :key="item.title">
+					<div class="title van-hairline--bottom">
+						<h2>{{ item.title }}</h2>
+					</div>
+					<div class="van-hairline--bottom common dec-name" v-for="content in item.dec_content">
+						{{ content.dec_name }}
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router'
-import { ref } from 'vue'
+import { useRoute } from "vue-router";
+import { ref } from "vue";
 
-import mgz_dec from "@/assets/magazine_test_dec.json"
-const route = useRoute()
-const ids = route.params.pubid.slice(0, -1).split('年')
-const year = ids[0]
-const issueNum = ids[1]
+import mgz_dec from "@/assets/magazine_test_dec.json";
+const route = useRoute();
+const ids = route.params.pubid.slice(0, -1).split("年");
+const year = ids[0];
+const issue = ids[1];
 
-let decList = ref([])
+let decList = ref([]);
 
-mgz_dec[year].some((item) => {
-    if (item.issue.slice(0, -1) == issueNum) {
-        decList.value = item.dec
-        return true
-    }
-
-})
+mgz_dec[year].some(item => {
+	if (item.issue.slice(0, -1) == issue) {
+		decList.value = item.dec;
+		return true;
+	}
+});
 console.log(mgz_dec[year]);
-
 </script>
 
 <style lang="less" scoped>
 .container {
-    height: 100%;
-    overflow-y: scroll;
-
-    h1 {
-        height: 61px;
-        line-height: 65px;
-        text-align: center;
-        background: url("../assets/w-mdicon4.gif") no-repeat center;
-        background-size: auto 61px;
-        font-size: 16px;
-    }
-
-    .dec-content {
-        padding: 0 16px;
-
-        ul {
-            display: grid;
-            row-gap: 15px;
-            font-size: 15px;
-
-            li {
-
-                .title {
-                    padding-bottom: 10px;
-
-                    h2 {
-                        margin: 0;
-                        font-size: 16px;
-                        display: flex;
-                        align-items: center;
-
-                        &::before {
-                            content: '';
-                            width: 4px;
-                            height: 18px;
-                            background-color: red;
-                            margin-right: 8px;
-
-                        }
-                    }
-                }
-
-                .dec-item {
-                    padding: 10px 0 10px 10px;
-                    display: flex;
-                    align-items: center;
-                    color: #333333;
-                    letter-spacing: 1px;
-                    line-height: 1.25;
-
-                    &::before {
-                        content: '';
-                        width: 3px;
-                        height: 3px;
-                        border-radius: 50%;
-                        background-color: rgb(191, 191, 191);
-                        margin-right: 8px;
-
-                    }
-                }
-            }
-        }
-    }
-
+	height: 100%;
+	overflow-y: scroll;
+	h1 {
+		height: 61px;
+		line-height: 65px;
+		text-align: center;
+		background: url("../assets/w-mdicon4.gif") no-repeat center;
+		background-size: auto 61px;
+		font-size: 16px;
+	}
+	.dec-content {
+		padding: 0 16px;
+		ul {
+			font-size: 15px;
+			li {
+				.title {
+					padding: 10px 0;
+					h2 {
+						display: flex;
+						align-items: center;
+						margin: 0;
+						font-size: 16px;
+						&::before {
+							content: "";
+							width: 4px;
+							height: 18px;
+							background-color: red;
+							margin-right: 8px;
+						}
+					}
+				}
+				.dec-name {
+					display: flex;
+					align-items: center;
+					padding: 10px 0 10px 10px;
+					letter-spacing: 1px;
+					line-height: 1.25;
+					color: #333333;
+					&::before {
+						content: "";
+						width: 3px;
+						height: 3px;
+						border-radius: 50%;
+						background-color: rgb(191, 191, 191);
+						margin-right: 8px;
+					}
+				}
+			}
+		}
+	}
 }
 </style>
--- src/page/MagazineList.vue
+++ src/page/MagazineList.vue
@@ -1,317 +1,298 @@
 <template>
-    <van-config-provider :theme-vars="themeVars" style="height: 100%">
-        <div class="container">
-            <div class="search-box">
-                <van-search placeholder="请输入期刊号" @focus="focus" @blur="blur" :shape="shape" />
-                <div class="tip-box " v-show="isShow">
-                    <div class="query-mode">
-                        <h3 class="tip-box-title">
-                            <span class="line"></span>
-                            期刊号输入格式
-                        </h3>
-                        <div class="fuzzy-query van-hairline--bottom">
-                            <h3>
-                                <span class="line"></span>
-                                模糊查询:
-                            </h3>
-                            <span>只需输入杂志的发行年份，可查询到该年份发行的所有期刊。</span>
-                        </div>
-                        <div class="exact-query">
-                            <h3>
-                                <span class="line"></span>
-                                精确查询:
-                            </h3>
-                            <span>输入发行年份+期号，例如:要查询1991年发行的01期期刊，只需输入199101即可查询到。</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-box" :class="{ bgc: !isShow, 'border-bottom': !isShow }">
-                <div class="top-box">
-                    <span :class="{ active: toggle }" @click="toggle = true">期刊</span>
-                    <span :class="{ active: !toggle }" @click="toggle = false">我的收藏</span>
-                </div>
-            </div>
-            <van-list v-model:loading="loading" :finished="finished" finished-text="没有更多了" @load="onLoad"
-                :immediate-check="false" offset="50" v-show="toggle">
-                <ul>
-                    <li v-for="item in result[0]" :key="item.publishDate"
-                        @click="$router.push(`/magazineInfo/${item.publishDate}`)">
-                        <div class="book-cover">
-                            <img v-lazy="item.img" alt="">
-                            <span class="bookCover_gradientDecor"></span>
-                        </div>
-                        <span class="issue">{{ item.publishDate }}</span>
-                    </li>
-                </ul>
-            </van-list>
+	<van-config-provider :theme-vars="themeVars" style="height: 100%">
+		<div class="container">
+			<div class="search-box">
+				<van-search placeholder="请输入期刊号" @focus="focus" @blur="blur" :shape="shape" />
+				<div class="tip-box" v-show="isShow">
+					<div class="query-mode">
+						<h3 class="tip-box-title">
+							<span class="line"></span>
+							期刊号输入格式
+						</h3>
+						<div class="fuzzy-query van-hairline--bottom">
+							<h3>
+								<span class="line"></span>
+								模糊查询:
+							</h3>
+							<span>只需输入杂志的发行年份，可查询到该年份发行的所有期刊。</span>
+						</div>
+						<div class="exact-query">
+							<h3>
+								<span class="line"></span>
+								精确查询:
+							</h3>
+							<span>输入发行年份+期号，例如:要查询1991年发行的01期期刊，只需输入199101即可查询到。</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="tab-box" :class="{ bgc: !isShow, 'border-bottom': !isShow }">
+				<div class="top-box">
+					<span :class="{ active: toggle }" @click="toggle = true">期刊</span>
+					<span :class="{ active: !toggle }" @click="toggle = false">我的收藏</span>
+				</div>
+			</div>
+			<van-list v-model:loading="loading" :finished="finished" finished-text="没有更多了" @load="onLoad" :immediate-check="false" offset="50" v-show="toggle">
+				<ul>
+					<li v-for="item in result[0]" :key="item.publishDate" @click="$router.push(`/magazineInfo/${item.publishDate}`)">
+						<div class="book-cover">
+							<img v-lazy="item.img" alt="" />
+							<span class="bookCover_gradientDecor"></span>
+						</div>
+						<span class="issue">{{ item.publishDate }}</span>
+					</li>
+				</ul>
+			</van-list>
 
-            <div style="padding-top: 102px" v-show="!toggle">暂无收藏</div>
-        </div>
-        <transition name="van-fade">
-            <div class="page-cover" v-show="isShow"></div>
-        </transition>
-    </van-config-provider>
+			<div style="padding-top: 102px" v-show="!toggle">暂无收藏</div>
+		</div>
+		<transition name="van-fade">
+			<div class="page-cover" v-show="isShow"></div>
+		</transition>
+	</van-config-provider>
 </template>
 
 <script setup>
-import { ref } from "vue"
-import { chunk, random } from 'lodash';
-import magazineList from "@/assets/magazine.json"
+import { ref } from "vue";
+import { chunk, random } from "lodash";
+import magazineList from "@/assets/magazine.json";
 
 const loading = ref(false);
 const finished = ref(false);
-const formatData = []
-const result = ref([])
-const isShow = ref(false)
-const shape = ref("round")
-const toggle = ref(true)
+const formatData = [];
+const result = ref([]);
+const isShow = ref(false);
+const shape = ref("round");
+const toggle = ref(true);
 
 magazineList.forEach(item => {
-    item.pubIssue.forEach(issueObj => {
-        const { pubYear } = item
-        const { issue, img } = issueObj
-        formatData.push({ publishDate: pubYear + issue, img })
-    })
-})
-result.value = chunk(formatData, 18)
+	item.pubIssue.forEach(issueObj => {
+		const { pubYear } = item;
+		const { issue, img } = issueObj;
+		formatData.push({ publishDate: pubYear + issue, img });
+	});
+});
+result.value = chunk(formatData, 18);
 
-let index = 0
+let index = 0;
 const onLoad = () => {
-    //模拟下拉加载
-    const randomNum = random(500, 1000)
-    setTimeout(() => {
-        index += 1
-        result.value[0].push(...result.value[index])
-        // 加载状态结束
-        loading.value = false;
-        // 数据全部加载完成
-        if (result.value[0].length >= formatData.length) {
-            finished.value = true;
-        }
-    }, randomNum);
+	//模拟下拉加载
+	const randomNum = random(500, 1000);
+	setTimeout(() => {
+		index += 1;
+		result.value[0].push(...result.value[index]);
+		// 加载状态结束
+		loading.value = false;
+		// 数据全部加载完成
+		if (result.value[0].length >= formatData.length) {
+			finished.value = true;
+		}
+	}, randomNum);
 };
 const themeVars = ref({
-    "searchInputHeight": '40px',
-    "searchBackgroundColor": 'rgb(244, 245, 247)',
-    "searchContentBackgroundColor": "#fff",
-    "searchLeftIconColor": "rgb(111, 120, 129)"
-})
+	searchInputHeight: "40px",
+	searchBackgroundColor: "rgb(244, 245, 247)",
+	searchContentBackgroundColor: "#fff",
+	searchLeftIconColor: "rgb(111, 120, 129)",
+});
 
 const focus = () => {
-    isShow.value = true
-    shape.value = "square"
-    themeVars.value.searchBackgroundColor = 'none'
-}
+	isShow.value = true;
+	shape.value = "square";
+	themeVars.value.searchBackgroundColor = "none";
+};
 
 const blur = () => {
-    isShow.value = false
-    shape.value = "round"
-    themeVars.value.searchBackgroundColor = 'rgb(244, 245, 247)'
-}
-
+	isShow.value = false;
+	shape.value = "round";
+	themeVars.value.searchBackgroundColor = "rgb(244, 245, 247)";
+};
 </script>
 
 <style lang="less" scoped>
 .page-cover {
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, .8);
+	position: fixed;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	background-color: rgba(0, 0, 0, 0.8);
 }
-
 .container {
-    width: 100%;
-    height: 100%;
-    overflow-y: scroll;
-    background-color: rgb(244, 245, 247);
+	width: 100%;
+	height: 100%;
+	overflow-y: scroll;
+	background-color: rgb(244, 245, 247);
+	//   chrome去除滚动条样式
+	&::-webkit-scrollbar {
+		display: none;
+	}
+	//   兼容火狐
+	scrollbar-width: none;
+	overflow: -moz-scrollbars-none;
+	//   兼容IE10+
+	-ms-overflow-style: none;
+	.search-box {
+		position: fixed;
+		width: 375px;
+		z-index: 9999;
 
-    //   chrome去除滚动条样式
-    &::-webkit-scrollbar {
-        display: none;
-    }
+		:deep(.van-search) {
+			.van-field__body {
+				input::placeholder {
+					color: rgb(111, 120, 129);
+				}
+			}
+		}
+		.tip-box {
+			position: absolute;
+			z-index: 999;
+			top: 45px;
+			padding-left: 12px;
+			padding-right: 12px;
+			h3 {
+				display: flex;
+				align-items: center;
+				font-weight: normal;
+				font-size: 15px;
+				color: #353c46;
+				.line {
+					width: 5px;
+					height: 18px;
+					background-color: red;
+					margin-right: 5px;
+				}
+			}
+			.query-mode {
+				display: grid;
+				row-gap: 10px;
+				background-color: white;
+				padding: 15px 10px;
+				border-bottom-left-radius: 2px;
+				border-bottom-right-radius: 2px;
 
-    //   兼容火狐
-    scrollbar-width: none;
-    overflow: -moz-scrollbars-none;
-    //   兼容IE10+
-    -ms-overflow-style: none;
+				h3 {
+					margin-top: 0px;
+					margin-bottom: 5px;
+					color: #0d141e;
+				}
 
-    .search-box {
-        position: fixed;
-        width: 375px;
-        z-index: 9999;
+				span {
+					display: block;
+					line-height: 1.5em;
+					color: #858c96;
+					font-size: 14px;
+				}
 
-        :deep(.van-search) {
-            .van-field__body {
-                input::placeholder {
-                    color: rgb(111, 120, 129);
-                }
+				.fuzzy-query {
+					padding-bottom: 10px;
 
-            }
-        }
+					h3 {
+						.line {
+							background-color: #353c46;
+						}
+					}
+				}
 
-        .tip-box {
-            position: absolute;
-            z-index: 999;
-            top: 45px;
-            padding-left: 12px;
-            padding-right: 12px;
-
-            h3 {
-                display: flex;
-                align-items: center;
-                font-weight: normal;
-                font-size: 15px;
-                color: #353c46;
-
-                .line {
-                    width: 5px;
-                    height: 18px;
-                    background-color: red;
-                    margin-right: 5px;
-                }
-            }
-
-            .query-mode {
-                display: grid;
-                row-gap: 10px;
-                background-color: white;
-                padding: 15px 10px;
-                border-bottom-left-radius: 2px;
-                border-bottom-right-radius: 2px;
-
-                h3 {
-                    margin-top: 0px;
-                    margin-bottom: 5px;
-                    color: #0d141e;
-
-                }
-
-                span {
-                    display: block;
-                    line-height: 1.5em;
-                    color: #858c96;
-                    font-size: 14px;
-                }
-
-                .fuzzy-query {
-                    padding-bottom: 10px;
-
-                    h3 {
-                        .line {
-                            background-color: #353c46;
-                        }
-                    }
-                }
-
-                .exact-query {
-                    h3 {
-                        .line {
-                            background-color: #086cf8;
-                        }
-                    }
-                }
-            }
-        }
-
-        @media screen and (min-width: 540px) {
-            .tip-box {
-                top: 37px
-            }
-        }
-    }
-
-    .tab-box {
-        display: grid;
-        position: fixed;
-        width: 375px;
-        padding-top: 65px;
-        padding-bottom: 15px;
-        // padding-left: 15px;
-        font-size: 15px;
-        color: rgb(133, 140, 150);
-        z-index: 999;
-
-        &.bgc {
-            background-color: #f4f5f7;
-        }
-
-        &.border-bottom {
-            border-bottom: 1px solid rgb(235, 234, 234);
-        }
-
-        .top-box {
-            // padding-bottom: 8px;
-            padding-left: 15px;
-
-            :nth-child(1) {
-                margin-right: 25px;
-            }
-
-            span {
-                &.active {
-                    color: rgb(25, 136, 236);
-                }
-            }
-        }
-    }
-
-    ul {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        gap: 10px;
-        justify-items: center;
-        padding: 112px 10px 0 10px;
-
-        li {
-            width: 105px;
-            border-radius: 5px;
-
-            .book-cover {
-                position: relative;
-                height: 146px;
-
-                .bookCover_gradientDecor {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    right: 0;
-                    bottom: 0;
-                    background-image: linear-gradient(90deg, hsla(0, 0%, 63.1%, .25), rgba(21, 21, 20, .1) 1%, hsla(0, 0%, 100%, .15) 4%, hsla(0, 0%, 58%, .1) 8%, hsla(0, 0%, 89%, 0) 57%, rgba(223, 218, 218, .03) 91%, rgba(223, 218, 218, .05) 98%, hsla(0, 0%, 100%, .1));
-                    box-shadow: inset 0 0 0 0 rgba(0, 0, 0, .1);
-                }
-
-                img {
-                    width: 100%;
-                    height: 100%;
-                    object-fit: cover;
-                    box-shadow: 0 2px 16px rgba(0, 0, 0, .08);
-                    background: #d8d8d8;
-                }
-            }
-
-            .issue {
-                display: block;
-                margin: 6px 0;
-                font-size: 15px;
-                text-align: center;
-                // color: #5d646e;
-                color: rgb(33, 40, 50);
-            }
-        }
-    }
-
-    @media screen and (min-width: 540px) {
-        .tab-box {
-            padding-top: 52px;
-        }
-
-        ul {
-            padding-top: 100px
-        }
-    }
+				.exact-query {
+					h3 {
+						.line {
+							background-color: #086cf8;
+						}
+					}
+				}
+			}
+		}
+		@media screen and (min-width: 540px) {
+			.tip-box {
+				top: 37px;
+			}
+		}
+	}
+	.tab-box {
+		display: grid;
+		position: fixed;
+		width: 375px;
+		padding-top: 65px;
+		padding-bottom: 15px;
+		// padding-left: 15px;
+		font-size: 15px;
+		color: rgb(133, 140, 150);
+		z-index: 999;
+		&.bgc {
+			background-color: #f4f5f7;
+		}
+		&.border-bottom {
+			border-bottom: 1px solid rgb(235, 234, 234);
+		}
+		.top-box {
+			padding-left: 15px;
+			:nth-child(1) {
+				margin-right: 25px;
+			}
+			span {
+				&.active {
+					color: rgb(25, 136, 236);
+				}
+			}
+		}
+	}
+	ul {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		gap: 10px;
+		justify-items: center;
+		padding: 112px 10px 0 10px;
+		li {
+			width: 105px;
+			border-radius: 5px;
+			.book-cover {
+				position: relative;
+				height: 146px;
+				.bookCover_gradientDecor {
+					position: absolute;
+					top: 0;
+					left: 0;
+					right: 0;
+					bottom: 0;
+					background-image: linear-gradient(
+						90deg,
+						hsla(0, 0%, 63.1%, 0.25),
+						rgba(21, 21, 20, 0.1) 1%,
+						hsla(0, 0%, 100%, 0.15) 4%,
+						hsla(0, 0%, 58%, 0.1) 8%,
+						hsla(0, 0%, 89%, 0) 57%,
+						rgba(223, 218, 218, 0.03) 91%,
+						rgba(223, 218, 218, 0.05) 98%,
+						hsla(0, 0%, 100%, 0.1)
+					);
+					box-shadow: inset 0 0 0 0 rgba(0, 0, 0, 0.1);
+				}
+				img {
+					width: 100%;
+					height: 100%;
+					object-fit: cover;
+					box-shadow: 0 2px 16px rgba(0, 0, 0, 0.08);
+					background: #d8d8d8;
+				}
+			}
+			.issue {
+				display: block;
+				margin: 6px 0;
+				font-size: 15px;
+				text-align: center;
+				// color: #5d646e;
+				color: rgb(33, 40, 50);
+			}
+		}
+	}
+	@media screen and (min-width: 540px) {
+		.tab-box {
+			padding-top: 52px;
+		}
+		ul {
+			padding-top: 100px;
+		}
+	}
 }
 </style>
